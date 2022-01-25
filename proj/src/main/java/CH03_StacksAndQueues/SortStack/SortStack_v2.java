a00a8e4cd52ca368fd1155c40e75e0a4738e6bda
hobossa <hoboss@gmail.com>
2022-01-25 01:25:27 -0800

    StacksAndQueues.SortStack. version 2. Omit some redundant code.

--- proj/src/main/java/CH03_StacksAndQueues/SortStack/SortStack_v2.java
+++ proj/src/main/java/CH03_StacksAndQueues/SortStack/SortStack_v2.java
@@ -11,11 +11,11 @@ public class SortStack_v2 {
         // keep the tempStack is sorted, which the biggest element is on the top.
         Stack<T> tempStack = new Stack<>();
         while (!stack.empty()) {
-            int n = 0; // number of moved element.
+            // int n = 0; // number of moved element.
             T e = stack.pop();
             while (!tempStack.empty()) {
                 if (e.compareTo(tempStack.peek()) < 0) {
-                    n++;
+                    // n++;
                     // use the original stack to temporary these elements which are bigger than e
                     stack.push(tempStack.pop());
                 } else {
@@ -24,10 +24,14 @@ public class SortStack_v2 {
             }
             // push e into tempStack
             tempStack.push(e);
+
+            // actually we don't need the following 3 lines, and the variable n
+            // because even if we don't do the following 3 line code,  at the next loop e.compareTo() < 0 is false
+            // and the code above will put these elements back.
             // push bigger elements back
-            while (n-- >0) {
-                tempStack.push(stack.pop());
-            }
+            // while (n-- >0) {
+            //     tempStack.push(stack.pop());
+            // }
         }
         // now the tempStack are sorted but the biggest element is on the top. we need to put is back to
         // the stack which is passed in as the argument.
