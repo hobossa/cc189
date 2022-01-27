10abbd23f8cf80c83d45d2f1e13faa7998a40658
hobossa <hoboss@gmail.com>
2022-01-27 10:55:18 -0800

    TreesAndGraphs.CheckBalanced. brute force. not efficient. subtrees' height will be calculate multi times.

---
+++ proj/src/main/java/CH04_TreesAndGraphs/CheckBalanced/CheckBalancedTest.java
@@ -0,0 +1,34 @@
+package CH04_TreesAndGraphs.CheckBalanced;
+
+// Implement a function to check if a binary tree is balanced. For the purposes of this question, a balanced tree
+// is defined to be a tree such that the heights of the two subtrees of any node never differ by more than one.
+// Hints: #21, #33, #49, #105, #124
+
+import CH04_TreesAndGraphs.ListOfDepths.ListOfDepths_v1;
+import Common.BiTreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class CheckBalancedTest {
+
+    public static void main(String[] args) {
+        int n = ThreadLocalRandom.current().nextInt(5, 15);
+        Integer[] arr = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = i;
+        }
+        BiTreeNode<Integer> tree = BiTreeNode.createBSTFromSortedArray(arr);
+        System.out.println(CheckBalanced_v1.isBalanced(tree));
+
+        BiTreeNode.insert(tree, 20);
+        BiTreeNode.insert(tree, 21);
+        BiTreeNode.insert(tree, 22);
+        System.out.println(CheckBalanced_v1.isBalanced(tree));
+        ArrayList<LinkedList<BiTreeNode<Integer>>> arrayList1 = BiTreeNode.toListOfDepth(tree);
+        for (int i = 0; i < arrayList1.size(); i++) {
+            System.out.println(arrayList1.get(i));
+        }
+    }
+}
---
+++ proj/src/main/java/CH04_TreesAndGraphs/CheckBalanced/CheckBalanced_v1.java
@@ -0,0 +1,18 @@
+package CH04_TreesAndGraphs.CheckBalanced;
+
+import Common.BiTreeNode;
+
+// brute force
+// It's not efficient, because the height of subtrees are calculated many times.
+public class CheckBalanced_v1 {
+    public static <E> boolean isBalanced(BiTreeNode<E> root) {
+        return Math.abs(height(root.left) - height(root.right)) <= 1;
+    }
+
+    private static <E> int height(BiTreeNode<E> root) {
+        if (null == root) {
+            return 0;
+        }
+        return 1 + Math.max(height(root.left), height(root.right));
+    }
+}
