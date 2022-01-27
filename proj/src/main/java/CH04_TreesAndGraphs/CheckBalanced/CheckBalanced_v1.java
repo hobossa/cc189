3919624f2bba57b4616f4a03a515dc88d79e9d4a
hobossa <hoboss@gmail.com>
2022-01-27 11:03:48 -0800

    CheckBalanced_v1. fix a bug.

--- proj/src/main/java/CH04_TreesAndGraphs/CheckBalanced/CheckBalanced_v1.java
+++ proj/src/main/java/CH04_TreesAndGraphs/CheckBalanced/CheckBalanced_v1.java
@@ -6,7 +6,10 @@ import Common.BiTreeNode;
 // It's not efficient, because the height of subtrees are calculated many times.
 public class CheckBalanced_v1 {
     public static <E> boolean isBalanced(BiTreeNode<E> root) {
-        return Math.abs(height(root.left) - height(root.right)) <= 1;
+        if (null == root) return true;
+        return isBalanced(root.left)
+                && isBalanced(root.right)
+                && (Math.abs(height(root.left) - height(root.right)) <= 1);
     }
 
     private static <E> int height(BiTreeNode<E> root) {
