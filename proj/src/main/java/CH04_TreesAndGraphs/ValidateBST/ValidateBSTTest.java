ea5afde6b4cea6bf9ac336d770c0c0ce05b6c28f
hobossa <hoboss@gmail.com>
2022-01-29 02:15:14 -0800

    ValidateBSAT, fix the bug
    In fact, There are two scenarios continuously alternating here.
    1, the lastElement is on my left child tree.
    2. current element is on the last Element's right child tree.

--- proj/src/main/java/CH04_TreesAndGraphs/ValidateBST/ValidateBSTTest.java
+++ proj/src/main/java/CH04_TreesAndGraphs/ValidateBST/ValidateBSTTest.java
@@ -5,37 +5,39 @@ package CH04_TreesAndGraphs.ValidateBST;
 
 import Common.BiTreeNode;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 // You should ask the interviewer that how to deal with duplicate values.
 // Here we assume, we only allow the duplicate values on the left side.
 public class ValidateBSTTest {
     public static void main(String[] args) {
         //int n = ThreadLocalRandom.current().nextInt(5, 15);
-        // int n = 6;
-        // Integer[] arr = new Integer[n];
-        // for (int i = 0; i < n; i++) {
-        //     arr[i] = i;
-        // }
-        // BiTreeNode<Integer> tree = BiTreeNode.createBSTFromSortedArray(arr);
-        // System.out.println(ValidateBST_v1.isBST(tree));
-        // System.out.println();
-        // System.out.println(ValidateBST_v2.isBST(tree));
-        // System.out.println();
-        // System.out.println(ValidateBST_v3.isBST(tree));
-        // System.out.println();
-        // System.out.println(ValidateBST_v4.isBST(tree));
-        // System.out.println();
-        //
-        // System.out.println();
-        // Integer[] arrEx = {7, 4, 6, 7, 12, 56, 8, 12, 9};
-        // BiTreeNode<Integer> tree2 = BiTreeNode.createBSTFromSortedArray(arrEx);
-        // System.out.println(ValidateBST_v1.isBST(tree2));
-        // System.out.println();
-        // System.out.println(ValidateBST_v2.isBST(tree2));
-        // System.out.println();
-        // System.out.println(ValidateBST_v3.isBST(tree2));
-        // System.out.println();
-        // System.out.println(ValidateBST_v4.isBST(tree2));
-        // System.out.println();
+        int n = 6;
+        Integer[] arr = new Integer[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = i;
+        }
+        BiTreeNode<Integer> tree = BiTreeNode.createBSTFromSortedArray(arr);
+        System.out.println(ValidateBST_v1.isBST(tree));
+        System.out.println();
+        System.out.println(ValidateBST_v2.isBST(tree));
+        System.out.println();
+        System.out.println(ValidateBST_v3.isBST(tree));
+        System.out.println();
+        System.out.println(ValidateBST_v4.isBST(tree));
+        System.out.println();
+
+        System.out.println();
+        Integer[] arrEx = {7, 4, 6, 7, 12, 56, 8, 12, 9};
+        BiTreeNode<Integer> tree2 = BiTreeNode.createBSTFromSortedArray(arrEx);
+        System.out.println(ValidateBST_v1.isBST(tree2));
+        System.out.println();
+        System.out.println(ValidateBST_v2.isBST(tree2));
+        System.out.println();
+        System.out.println(ValidateBST_v3.isBST(tree2));
+        System.out.println();
+        System.out.println(ValidateBST_v4.isBST(tree2));
+        System.out.println();
 
         System.out.println();
         Integer[] arr3 = {1, 2, 3, 4, 4, 5, 6};
--- proj/src/main/java/CH04_TreesAndGraphs/ValidateBST/ValidateBST_v3.java
+++ proj/src/main/java/CH04_TreesAndGraphs/ValidateBST/ValidateBST_v3.java
@@ -4,6 +4,7 @@ import Common.BiTreeNode;
 
 public class ValidateBST_v3 {
     static Object lastElement = null;
+    static boolean isLastElementInLeftChildTree = true;
 
     // solution from book
     // traverse the tree using an in-order traversal (left, current, right).
@@ -11,37 +12,38 @@ public class ValidateBST_v3 {
     // when it is go right child, the last node should not be equal to or greater than current node.
 
     public static <E extends Comparable<? super E>> boolean isBST(BiTreeNode<E> root) {
-        return isBSTInner(root, true);
+        return isBSTInner(root);
     }
 
-    public static <E extends Comparable<? super E>> boolean isBSTInner(BiTreeNode<E> node, boolean goLeft) {
+    public static <E extends Comparable<? super E>> boolean isBSTInner(BiTreeNode<E> node) {
 
         if (null == node) {
             return true;
         }
 
         // goLeft
-        if (!isBSTInner(node.left, true)) {
+        if (!isBSTInner(node.left)) {
             return false;
         }
 
         if (null != lastElement) {
             // System.out.println(lastElement + " " + goLeft + " " + node.element);
-            if (goLeft) {
-                // the lastNode is on my left child tree
+            if (isLastElementInLeftChildTree) {
+                // the lastNode is on current node's left child tree
                 if (((E) lastElement).compareTo(node.element) > 0) {
                     return false;
                 }
             } else {
-                // the lastNode is my parent
+                // current node is on the last Element's right child tree.
                 if (((E) lastElement).compareTo(node.element) >= 0) {
                     return false;
                 }
             }
+            isLastElementInLeftChildTree = !isLastElementInLeftChildTree;
         }
         lastElement = node.element;
 
-        if (!isBSTInner(node.right, false)) {
+        if (!isBSTInner(node.right)) {
             return false;
         }
 
