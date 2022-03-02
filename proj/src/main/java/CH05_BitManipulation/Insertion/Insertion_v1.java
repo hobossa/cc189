0e4ec2bb0627b486c5b436d30d56af88f48b5832
hobossa <hoboss@gmail.com>
2022-03-02 14:14:55 -0800

    BitManipulation.Insertion. add validation

--- proj/src/main/java/CH05_BitManipulation/Insertion/Insertion_v1.java
+++ proj/src/main/java/CH05_BitManipulation/Insertion/Insertion_v1.java
@@ -4,6 +4,11 @@ public class Insertion_v1 {
     // time O(j-i)
     // In fact, we don't need to do it with a loop.
     public static int insertion(int n, int m, int i, int j) {
+        // Validation
+        if (i > j || i < 0 || j >= 32) {
+            return 0;
+        }
+
         int t = 0;
         while (i <= j) {
             boolean b = getBit(m, t);
--- proj/src/main/java/CH05_BitManipulation/Insertion/Insertion_v2.java
+++ proj/src/main/java/CH05_BitManipulation/Insertion/Insertion_v2.java
@@ -2,6 +2,11 @@ package CH05_BitManipulation.Insertion;
 
 public class Insertion_v2 {
     public static int insertion(int n, int m, int i, int j) {
+        // Validation
+        if (i > j || i < 0 || j >= 32) {
+            return 0;
+        }
+
         int mask = -1 << i;
         //System.out.println(Integer.toBinaryString(mask));
         if (j < 31) {
