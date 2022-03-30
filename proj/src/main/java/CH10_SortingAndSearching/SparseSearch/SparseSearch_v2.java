bddea05359b56e09d4e482ffc32d7a8ffae3747f
hobossa <hoboss@gmail.com>
2022-03-30 08:20:04 -0700

    Update SparseSearch_v2.java
    OPT: better code to find the no-empty middle string.

--- proj/src/main/java/CH10_SortingAndSearching/SparseSearch/SparseSearch_v2.java
+++ proj/src/main/java/CH10_SortingAndSearching/SparseSearch/SparseSearch_v2.java
@@ -14,30 +14,48 @@ public class SparseSearch_v2 {
         }
         int n = (s + e) / 2;
         // find the nearest no-empty string.
-        int i = 0;
-        while (true) {
-            if (n-i >= s && n-i <= e && !array[n-i].isEmpty()) {
-                n = n-i;
-                break;
-            }
-            if (n+i >=s && n+i <= e && !array[n+i].isEmpty()) {
-                n = n+i;
-                break;
-            }
-            i++;
-        }
+        // int i = 0;
+        // while (true) {
+        //     if (n-i >= s && n-i <= e && !array[n-i].isEmpty()) {
+        //         n = n-i;
+        //         break;
+        //     }
+        //     if (n+i >=s && n+i <= e && !array[n+i].isEmpty()) {
+        //         n = n+i;
+        //         break;
+        //     }
+        //     i++;
+        // }
+        //
+        // if (n < s || n > e) {
+        //     return -1;
+        // }
 
-        if (n < s || n > e) {
-            return -1;
+        // find the nearest no-empty, the following code is from book which is better
+        if (array[n].isEmpty()) {
+            int left = n - 1;
+            int right = n + 1;
+            while (true) {
+                if (left < s && right > e) {
+                    return -1;
+                } else if (left >= s && !array[left].isEmpty()) {
+                    n = left;
+                    break;
+                } else if (right <= e && !array[right].isEmpty()) {
+                    n = right;
+                    break;
+                }
+                left--;
+                right++;
+            }
         }
 
         if (array[n].compareTo(str) == 0) {
             return n;
+        } else if (array[n].compareTo(str) > 0) {
+            return searchHelper(array, str, s, n - 1);
+        } else {
+            return searchHelper(array, str, n + 1, e);
         }
-
-        if (array[n].compareTo(str) > 0) {
-            return searchHelper(array, str, s, n-1);
-        }
-        return searchHelper(array, str, n+1, e);
     }
 }
